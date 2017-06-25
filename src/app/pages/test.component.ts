c32a3affeb58ce73942ae83f239cb52a26773a77
AliObaji <ali-obaji@hotmail.com>
2017-06-25 15:34:18 +0300

    Moved styles into app for compilation.

---
+++ src/app/pages/test.component.ts
@@ -0,0 +1,15 @@
+/**
+ * Created by Lenovo on 25-Jun-17.
+ */
+import { Component, ViewEncapsulation } from "@angular/core";
+
+@Component({
+  selector: "test",
+  templateUrl: "./test.component.html",
+  styleUrls: ["./test.component.scss"],
+  encapsulation: ViewEncapsulation.None
+})
+
+export class testComponent{
+
+}
