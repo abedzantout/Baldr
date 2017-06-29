a9a7fa226aa8e7b23f5fbb0067ceb5d69cbf7f58
AliObaji <ali-obaji@hotmail.com>
2017-06-29 18:06:40 +0300

    Added 'components page' and 'design' empty components.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,11 +6,31 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { IntroductionComponent } from './components/overview/Introduction/introduction.component';
 import { codeGuidelinesComponent } from './components/overview/code-guidelines/code-guidelines.component';
+import { buttonsPageComponent } from './components/components-page/buttons/buttons.component';
+import { FormElementsComponent } from './components/components-page/form-elements/form-elements.component';
+import { GridComponent } from './components/components-page/grid/grid.component';
+import { ListsComponent } from './components/components-page/lists/lists.component';
+import { ColorSchemeComponent } from './components/design/color-scheme/color-scheme.component';
+import { TypographyComponent } from './components/design/typography/typography.component';
+import { BorderComponent } from './components/design/border/border.component';
+import { BoxShadowComponent } from './components/design/box-shadow/box-shadow.component';
+import { LayoutComponent } from './components/design/layout/layout.component';
+import { IconsComponent } from './components/design/icons/icons.component';
 
 const routes: Routes = [
   {path:'',redirectTo:'introduction',pathMatch:'full'},
   {path:'introduction',component: IntroductionComponent},
-  {path:'code-guidelines',component: codeGuidelinesComponent}
+  {path:'code-guidelines',component: codeGuidelinesComponent},
+  {path:'buttons',component: buttonsPageComponent},
+  {path:'form-elements',component: FormElementsComponent},
+  {path:'grid',component: GridComponent},
+  {path:'lists',component: ListsComponent},
+  {path:'color-scheme',component: ColorSchemeComponent},
+  {path:'typography',component: TypographyComponent},
+  {path:'borders',component: BorderComponent},
+  {path:'box-shadows',component: BoxShadowComponent},
+  {path:'layout',component: LayoutComponent},
+  {path:'icons',component: IconsComponent}
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,13 +12,33 @@ import { AppComponent } from './app.component';
 import { sideNavComponent } from './components/side-nav/side-nav.component';
 import { IntroductionComponent } from './components/overview/Introduction/introduction.component';
 import { codeGuidelinesComponent } from './components/overview/code-guidelines/code-guidelines.component';
+import { buttonsPageComponent } from './components/components-page/buttons/buttons.component';
+import { FormElementsComponent } from './components/components-page/form-elements/form-elements.component';
+import { GridComponent } from './components/components-page/grid/grid.component';
+import { ListsComponent } from './components/components-page/lists/lists.component';
+import { ColorSchemeComponent } from './components/design/color-scheme/color-scheme.component';
+import { TypographyComponent } from './components/design/typography/typography.component';
+import { BorderComponent } from './components/design/border/border.component';
+import { BoxShadowComponent } from './components/design/box-shadow/box-shadow.component';
+import { LayoutComponent } from './components/design/layout/layout.component';
+import { IconsComponent } from './components/design/icons/icons.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     sideNavComponent,
     IntroductionComponent,
-    codeGuidelinesComponent
+    codeGuidelinesComponent,
+    buttonsPageComponent,
+    FormElementsComponent,
+    GridComponent,
+    ListsComponent,
+    ColorSchemeComponent,
+    TypographyComponent,
+    BorderComponent,
+    BoxShadowComponent,
+    LayoutComponent,
+    IconsComponent
   ],
   imports: [
     BrowserModule,
