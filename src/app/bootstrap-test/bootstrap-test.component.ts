19b3797f306546f7e3563896475abd8a9eaddb12
AliObaji <ali-obaji@hotmail.com>
2017-07-14 15:48:15 +0300

    Styled simple input fields.

--- src/app/bootstrap-test/bootstrap-test.component.ts
+++ src/app/bootstrap-test/bootstrap-test.component.ts
@@ -68,6 +68,26 @@ import { Component } from '@angular/core';
     <a class="dropdown-item" href="#">Something else here</a>
   </div>
 </div>
+
+<form class="form-inline">
+  <label class="mr-sm-2" for="inlineFormCustomSelect">Preference</label>
+  <select class="custom-select mb-2 mr-sm-2 mb-sm-0" id="inlineFormCustomSelect">
+    <option selected>Choose...</option>
+    <option value="1">One</option>
+    <option value="2">Two</option>
+    <option value="3">Three</option>
+  </select>
+
+  <label class="custom-control custom-checkbox mb-2 mr-sm-2 mb-sm-0">
+    <input type="checkbox" class="custom-control-input">
+    <span class="custom-control-indicator"></span>
+    <span class="custom-control-description">Remember my preference</span>
+  </label>
+
+  <button type="submit" class="btn btn-primary">Submit</button>
+</form>
+
+
           </nga-layout-column>
       </nga-layout>
   `,
